43ebbb134fe3327bfee7bf6b6eeee4d6fab9cd13
Nikita B. Zuev <nikitazu@gmail.com>
2015-11-12 23:18:06 +0300

    Refactor

--- spec/parse_sberbank_html_tests/html_parser_tests.rb
+++ spec/parse_sberbank_html_tests/html_parser_tests.rb
@@ -2,22 +2,21 @@
 require 'parse_sberbank_html'
 
 describe ParseSberbankHtml::HtmlParser do
-  let(:parser) { ParseSberbankHtml::HtmlParser.new }
-  let(:html) { File.read("#{File.dirname(__FILE__)}/data/html_parser_input.html") }
-  
   describe '#parse' do
-    it 'handles empty input' do
-      result = parser.parse ""
-      result[:transfers].length.should == 0
+    context 'when empty' do
+      let(:result) { subject.parse '' }
+      it { result[:transfers].length.should == 0 }
     end
     
-    it 'handles html input' do
-      result = parser.parse html
-      result[:transfers].length.should == 1
-      result[:transfers][0].should == {
+    context 'when not empty' do
+      let(:html) { File.read("#{File.dirname(__FILE__)}/data/html_parser_input.html") }
+      let(:result) { subject.parse html }
+      it { result[:transfers].length.should == 1 }
+      it { result[:transfers][0].should == {
         title: "SUSHI SHOP SANKT-PETERBU RUS",
         date: "17АВГ",
         amount: "123.00CR RUR" }
+      }
     end
   end
 end
--- spec/parse_sberbank_html_tests/text_parser_tests.rb
+++ spec/parse_sberbank_html_tests/text_parser_tests.rb
@@ -2,11 +2,10 @@
 require 'parse_sberbank_html'
 
 describe ParseSberbankHtml::TextParser do
-  let(:parser) { ParseSberbankHtml::TextParser.new }
-  
   describe '#parse' do
     context 'when empty' do
-      it { parser.parse("")[:transfers].length.should == 0 }
+      let(:result) { subject.parse '' }
+      it { result[:transfers].length.should == 0 }
     end
     
     context 'when no headers' do
@@ -14,7 +13,7 @@ describe ParseSberbankHtml::TextParser do
   ATM RUS SANKT-PETERBU ATM 123456	20.02 	−3 000,00 руб.	 
   BP Acct - Card RUS SBERBANK ONL@IN VKLAD-KARTA	20.02 	80 000,00 руб."
 
-      subject(:result) { parser.parse(text) }
+      let(:result) { subject.parse text }
 
       it { result[:transfers].length.should == 2 }
       it { result[:transfers][0].should == { 
@@ -35,7 +34,7 @@ describe ParseSberbankHtml::TextParser do
   ATM RUS SANKT-PETERBU ATM 239492	20.02	−20 000,00 руб.
   BP Acct - Card RUS SBERBANK ONL@IN VKLAD-KARTA	20.02	+10 000,00 руб."
 
-      subject(:result) { parser.parse(text) }
+      let(:result) { subject.parse text }
 
       it { result[:transfers].length.should == 2 }
       it { result[:transfers][0].should == { 

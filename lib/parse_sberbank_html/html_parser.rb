e857ad609b77503a8173aaf9ef88ad566bf7b994
Nikita B. Zuev <nikitazu@gmail.com>
2015-11-12 22:42:57 +0300

    Html parsing

--- lib/parse_sberbank_html/html_parser.rb
+++ lib/parse_sberbank_html/html_parser.rb
@@ -1,8 +1,21 @@
 # encoding: utf-8
+
+require 'nokogiri'
+
 module ParseSberbankHtml
   class HtmlParser
     def parse html
       transfers = []
+      doc = Nokogiri::HTML html
+      transfer_nodes = doc.css '.rowTrnData'
+      transfer_nodes.each do |node|
+        data_cells = node.css 'td'
+        tr = { 
+          title: data_cells[4].text,
+          date: data_cells[1].text,
+          amount: "#{data_cells[7].text} #{data_cells[5].text}" }
+        transfers.push tr
+      end
       { transfers: transfers }
     end
   end # HtmlParser
--- spec/parse_sberbank_html_tests/html_parser_tests.rb
+++ spec/parse_sberbank_html_tests/html_parser_tests.rb
@@ -3,11 +3,21 @@ require 'parse_sberbank_html'
 
 describe ParseSberbankHtml::HtmlParser do
   let(:parser) { ParseSberbankHtml::HtmlParser.new }
+  let(:html) { File.read("#{File.dirname(__FILE__)}/data/html_parser_input.html") }
   
   describe '#parse' do
     it 'handles empty input' do
       result = parser.parse ""
       result[:transfers].length.should == 0
+    end
+    
+    it 'handles html input' do
+      result = parser.parse html
+      result[:transfers].length.should == 1
+      result[:transfers][0].should == {
+        title: "SUSHI SHOP SANKT-PETERBU RUS",
+        date: "17АВГ",
+        amount: "123.00CR RUR" }
     end
   end
 end

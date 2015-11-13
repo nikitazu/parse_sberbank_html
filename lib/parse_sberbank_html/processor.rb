d3e535bc60c242691bb49440bb35a0ab5578e18f
Nikita B. Zuev <nikitazu@gmail.com>
2015-11-14 02:07:52 +0300

    Refactor

--- lib/parse_sberbank_html/processor.rb
+++ lib/parse_sberbank_html/processor.rb
@@ -1,9 +1,8 @@
 # encoding: utf-8
 module ParseSberbankHtml
   class Processor
-    attr_accessor :settings
-    
     def initialize
+      @settings = nil
       @formats = {
         date: ParseSberbankHtml::Formats::HtmlDateFormat.new,
         currency: ParseSberbankHtml::Formats::TextCurrencyFormat.new,
@@ -11,17 +10,23 @@ module ParseSberbankHtml
       }
     end
     
+    def settings
+      @settings
+    end
+    
+    def settings= value
+      @settings = value
+      @formats.values.each {|k| k.settings = value }
+    end
+    
     def process data
-      @formats[:date].settings = self.settings
-      @formats[:type].settings = self.settings
-      @formats[:currency].settings = self.settings
       transfers = []
       unless data.nil? or data[:transfers].nil?
         data[:transfers].each do |transfer|
           date = @formats[:date].parse transfer[:date]
           amount_type, amount_data = @formats[:type].parse transfer[:amount].strip
           amount_currency, amount_data = @formats[:currency].parse amount_data
-          self.settings[:thousands_separators].each { |s| amount_data.delete! s }
+          @settings[:thousands_separators].each { |s| amount_data.delete! s }
           amount_value = amount_data.to_f
 
           transfers.push(

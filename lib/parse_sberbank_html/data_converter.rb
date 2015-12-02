a53adedf54f1ed2a0991157307a0ddc73c5f1ca0
Nikita B. Zuev <nikitazu@gmail.com>
2015-12-02 23:33:53 +0300

    Error reporting from data converter

--- lib/parse_sberbank_html/data_converter.rb
+++ lib/parse_sberbank_html/data_converter.rb
@@ -8,7 +8,13 @@ module ParseSberbankHtml
       source_key = self.settings[:source]
       target_key = self.settings[:target]
       source_value = source[source_key]
-      target_value = self.settings[:formats][target_key].parse source_value
+      target_value = nil
+      begin
+        target_value = self.settings[:formats][target_key].parse source_value
+      rescue
+        target[:errors] = {} if target[:errors].nil?
+        target[:errors][target_key] = source_key
+      end
       target[target_key] = target_value
     end
   end # DataConverter
--- spec/parse_sberbank_html_tests/data_converter_spec.rb
+++ spec/parse_sberbank_html_tests/data_converter_spec.rb
@@ -2,6 +2,14 @@
 require 'parse_sberbank_html'
 require 'spec_helper'
 
+class FailingFormat
+  attr_accessor :settings
+  
+  def parse value
+    raise 'Failure'
+  end
+end # FailingFormat
+
 describe ParseSberbankHtml::DataConverter do
   converter = ParseSberbankHtml::DataConverter.new
   describe '#convert' do
@@ -12,6 +20,15 @@ describe ParseSberbankHtml::DataConverter do
       converter.settings = { source: :name, target: :person, formats: formats }
       converter.convert source, target
       it { expect(target).to eq({ person: 'Mike' }) }
+    end
+    
+    context 'when fails' do
+      source = { name: 'Mike' }
+      target = { }
+      formats = { person: FailingFormat.new }
+      converter.settings = { source: :name, target: :person, formats: formats }
+      converter.convert source, target
+      it { expect(target).to eq({ person: nil, errors: { person: :name } }) }
     end
   end
 end
